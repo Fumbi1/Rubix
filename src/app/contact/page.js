8a7c10dbb0654d3d4d9c85bea0e51d5d52085bcd
Fumbi1 <olumuyiwafumbi@gmail.com>
2023-09-07 02:37:07 +0100

    Rectified the home page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,68 +1,68 @@
-// "use client";
-// import "./contactt.css";
-// import React, { useState, useRef } from "react";
-// import { useRouter } from "next/navigation";
-// import emailjs from "@emailjs/browser";
-// import Swal from 'sweetalert2';
-// import Transition from "../Transition/page";
+"use client";
+import "./contactt.css";
+import React, { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+import emailjs from "@emailjs/browser";
+import Swal from 'sweetalert2';
+import Transition from "../Transition/page";
 
-// const Contact = () => {
-//   const form = useRef();
-//   const routes = useRouter();
+const Contact = () => {
+  const form = useRef();
+  const routes = useRouter();
 
-//   const [inputTag, setInputTag] = React.useState(true);
-//   const [inputTag2, setInputTag2] = React.useState(true);
-//   const [inputValue, setInputValue] = React.useState("");
-//   const [inputValue2, setInputValue2] = React.useState("");
+  const [inputTag, setInputTag] = React.useState(true);
+  const [inputTag2, setInputTag2] = React.useState(true);
+  const [inputValue, setInputValue] = React.useState("");
+  const [inputValue2, setInputValue2] = React.useState("");
 
-//   const check = () => {
-//     setInputTag(false);
+  const check = () => {
+    setInputTag(false);
+  };
+
+  const check2 = (e) => {
+    setInputTag(true);
+    setInputValue("");
+  };
+
+  const check3 = () => {
+    setInputTag2(false);
+  };
+
+  const check4 = () => {
+    setInputTag2(true);
+    setInputValue2("");
+  };
+
+  const clear = (e) => {
+    setInputValue(e.target.value);
+  };
+activate upon launch
+  const clear2 = (e) => {
+    setInputValue2(e.target.value);
+  };
+
+// This is to console.log th form content
+//   const [details, setDetails] = useState({
+//     name: "",
+//     number: "",
+//     email: "",
+//     address: "",
+//     project: "",
+//     project_other: "",
+//     project2: "",
+//     project2_other: "",
+//     preference: "",
+//     start: "",
+//     budget: "",
+//   });
+
+//   const HandleChange = (e) => {
+//     const name = e.target.name;
+//     const value = e.target.value;
+//     setDetails((prev) => {
+//       return { ...prev, [name]: value };
+//     });
 //   };
-
-//   const check2 = (e) => {
-//     setInputTag(true);
-//     setInputValue("");
-//   };
-
-//   const check3 = () => {
-//     setInputTag2(false);
-//   };
-
-//   const check4 = () => {
-//     setInputTag2(true);
-//     setInputValue2("");
-//   };
-
-//   const clear = (e) => {
-//     setInputValue(e.target.value);
-//   };
-//activate upon launch
-//   const clear2 = (e) => {
-//     setInputValue2(e.target.value);
-//   };
-
-// // This is to console.log th form content
-// //   const [details, setDetails] = useState({
-// //     name: "",
-// //     number: "",
-// //     email: "",
-// //     address: "",
-// //     project: "",
-// //     project_other: "",
-// //     project2: "",
-// //     project2_other: "",
-// //     preference: "",
-// //     start: "",
-// //     budget: "",
-// //   });
-
-// //   const HandleChange = (e) => {
-// //     const name = e.target.name;
-// //     const value = e.target.value;
-// //     setDetails((prev) => {
-// //       return { ...prev, [name]: value };
-// //     });
-// //   };
 
 //   const FormHandler = (e) => {
 //     e.preventDefault();
@@ -123,271 +123,273 @@
 //     // console.log(details);
 //   };
 
-//   return (
-//     <div>
-//       <Transition>
-//         <form ref={form} onSubmit={FormHandler}>
-//           <p className="form_header">
-//             Have or need a creative project?  <br />Let's have a productive talk.
-//           </p>
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Full Name
-//             </label>
-//             <br />
-//             <input
-//               type="text"
-//               name="name"
-//               className="form_ans"
-//               placeholder="e.g, John Doe Wesley"
+  return (
+    <div>
+      <Transition>
+
         
-//               required
-//             />
-//           </div>
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Contact Number
-//             </label>
-//             <br />
-//             <input
-//               type="number"
-//               name="number"
-//               className="form_ans"
+        <form ref={form} >
+          <p className="form_header">
+            Have or need a creative project?  <br />Let's have a productive talk.
+          </p>
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Full Name
+            </label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              className="form_ans"
+              placeholder="e.g, John Doe Wesley"
         
-//               placeholder=""
-//               required
-//             />
-//           </div>
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Email Address
-//             </label>
-//             <br />
-//             <input
-//               type="email"
-//               name="email"
-//               className="form_ans"
+              required
+            />
+          </div>
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Contact Number
+            </label>
+            <br />
+            <input
+              type="number"
+              name="number"
+              className="form_ans"
         
-//               placeholder=""
-//               required
-//             />
-//           </div>
+              placeholder=""
+              required
+            />
+          </div>
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Email Address
+            </label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              className="form_ans"
+        
+              placeholder=""
+              required
+            />
+          </div>
           
-//           <div className="form_div">
-//             <fieldset>
-//               <legend for="" className="form_opt_title">
-//                 Type of Project
-//               </legend>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt1"
-//                 value="Residential Space"
+          <div className="form_div">
+            <fieldset>
+              <legend for="" className="form_opt_title">
+                Type of Project
+              </legend>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt1"
+                value="Residential Space"
         
-//                 onChange={check2}
-//                 name="project"
-//                 required
-//               />
-//               <label className="radio_title" for="opt1">
-//                 Residential Space
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt2"
-//                 value="Commercial Space"
+                onChange={check2}
+                name="project"
+                required
+              />
+              <label className="radio_title" for="opt1">
+                Residential Space
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt2"
+                value="Commercial Space"
         
-//                 onChange={check2}
-//                 name="project"
-//                 required
-//               />
-//               <label className="radio_title" for="opt2">
-//                 Commercial Space
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt3"
-//                 value="Other"
+                onChange={check2}
+                name="project"
+                required
+              />
+              <label className="radio_title" for="opt2">
+                Commercial Space
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt3"
+                value="Other"
         
-//                 onChange={check}
-//                 name="project"
-//                 required
-//               />
-//               <label className="radio_title" for="opt3">
-//                 Other
-//               </label>
-//               <input
-//                 type="text"
-//                 id="opt3"
-//                 name="project_other"
-//                 className="form_ans"
-//                 onChange={clear}
-//                 placeholder="Choose 'Other' to access"
-//                 value={inputValue}
-//                 disabled={inputTag}
-//                 required
-//               />
-//             </fieldset>
-//           </div>
-//           <div className="form_div">
-//             <fieldset>
-//               <legend for="" className="form_opt_title">
-//                 Scope of Project
-//               </legend>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt4"
-//                 value="Residential Space"
+                onChange={check}
+                name="project"
+                required
+              />
+              <label className="radio_title" for="opt3">
+                Other
+              </label>
+              <input
+                type="text"
+                id="opt3"
+                name="project_other"
+                className="form_ans"
+                onChange={clear}
+                placeholder="Choose 'Other' to access"
+                value={inputValue}
+                disabled={inputTag}
+                required
+              />
+            </fieldset>
+          </div>
+          <div className="form_div">
+            <fieldset>
+              <legend for="" className="form_opt_title">
+                Scope of Project
+              </legend>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt4"
+                value="Residential Space"
         
-//                 name="project2"
-//                 onChange={check4}
-//                 required
-//               />
-//               <label className="radio_title" for="opt4">
-//                 Renovation
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt5"
-//                 value="New Construction"
+                name="project2"
+                onChange={check4}
+                required
+              />
+              <label className="radio_title" for="opt4">
+                Renovation
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt5"
+                value="New Construction"
         
-//                 name="project2"
-//                 onChange={check4}
-//                 required
-//               />
-//               <label className="radio_title" for="opt5">
-//                 New Construction
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt6"
-//                 value="Furnishing and Styling"
+                name="project2"
+                onChange={check4}
+                required
+              />
+              <label className="radio_title" for="opt5">
+                New Construction
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt6"
+                value="Furnishing and Styling"
         
-//                 name="project2"
-//                 onChange={check4}
-//                 required
-//               />
-//               <label className="radio_title" for="opt6">
-//                 Furnishing and Styling
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt7"
-//                 value="3D design"
+                name="project2"
+                onChange={check4}
+                required
+              />
+              <label className="radio_title" for="opt6">
+                Furnishing and Styling
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt7"
+                value="3D design"
         
-//                 name="project2"
-//                 onChange={check4}
-//                 required
-//               />
-//               <label className="radio_title" for="opt7">
-//                 3D design
-//               </label>
-//               <br />
-//               <input
-//                 type="radio"
-//                 className="radio_tag"
-//                 id="opt8"
-//                 value="Other2"
+                name="project2"
+                onChange={check4}
+                required
+              />
+              <label className="radio_title" for="opt7">
+                3D design
+              </label>
+              <br />
+              <input
+                type="radio"
+                className="radio_tag"
+                id="opt8"
+                value="Other2"
         
-//                 name="project2"
-//                 onChange={check3}
-//                 required
-//               />
-//               <label className="radio_title" for="opt6">
-//                 Other
-//               </label>
-//               <input
-//                 type="text"
-//                 disabled={inputTag2}
-//                 name="project2_other"
+                name="project2"
+                onChange={check3}
+                required
+              />
+              <label className="radio_title" for="opt6">
+                Other
+              </label>
+              <input
+                type="text"
+                disabled={inputTag2}
+                name="project2_other"
         
-//                 value={inputValue2}
-//                 placeholder="Choose 'Other' to access"
-//                 onChange={clear2}
-//                 className="form_ans"
-//                 id="opt8"
-//                 required
-//               />
-//             </fieldset>
-//           </div>
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Brief detail about the Project
-//             </label>
-//             <br />
-//             <textarea
-//               rows="4"
-//               cols="40"
-//               name="preference"
-//               className="form_ans"
+                value={inputValue2}
+                placeholder="Choose 'Other' to access"
+                onChange={clear2}
+                className="form_ans"
+                id="opt8"
+                required
+              />
+            </fieldset>
+          </div>
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Brief detail about the Project
+            </label>
+            <br />
+            <textarea
+              rows="4"
+              cols="40"
+              name="preference"
+              className="form_ans"
         
-//               placeholder=""
-//               required
-//             />
-//           </div>
+              placeholder=""
+              required
+            />
+          </div>
 
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Project Address
-//             </label>
-//             <br />
-//             <input
-//               type="location"
-//               name="address"
-//               className="form_ans"
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Project Address
+            </label>
+            <br />
+            <input
+              type="location"
+              name="address"
+              className="form_ans"
         
-//               placeholder=""
-//               required
-//             />
-//           </div>
+              placeholder=""
+              required
+            />
+          </div>
 
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               When you will like the project to start
-//             </label>
-//             <br />
-//             <input
-//               type="date"
-//               name="start"
-//               className="form_ans"
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              When you will like the project to start
+            </label>
+            <br />
+            <input
+              type="date"
+              name="start"
+              className="form_ans"
         
-//               placeholder=""
-//               required
-//             />
-//           </div>
-//           <div className="form_div">
-//             <label for="" className="form_opt_title">
-//               Budget for this Project (NGN)
-//             </label>
-//             <br />
-//             <input
-//               type="text"
-//               name="budget"
-//               className="form_ans"
+              placeholder=""
+              required
+            />
+          </div>
+          <div className="form_div">
+            <label for="" className="form_opt_title">
+              Budget for this Project (NGN)
+            </label>
+            <br />
+            <input
+              type="text"
+              name="budget"
+              className="form_ans"
         
-//               placeholder="e.g, 1,500,000"
-//               required
-//             />
-//           </div>
-//           <button type="submit" className="contact_btn">
-//             Schedule a Consultation
-//           </button>
-//         </form>
-//       </Transition>
-//     </div>
-//   );
-// };
+              placeholder="e.g, 1,500,000"
+              required
+            />
+          </div>
+          <button type="submit" className="contact_btn">
+            Schedule a Consultation
+          </button>
+        </form>
+      </Transition>
+    </div>
+  );
+};
 
-// export default Contact;
+export default Contact;
